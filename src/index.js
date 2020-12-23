66a9b89bcf64cfbfb5dcbbb82cca70b95390fe9b
Nikita Stolbovskih <stolbovskikh@speechpro.com>
2020-12-23 23:30:31 +0300

    add showing weather from input

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 // my key : 458b6f24d9bec1db57c2f337dc35dbcc
 
+(async function () {
+  const localWeather = await getLocalWeather();
+
+  const div = document.querySelector(".weather");
+
+  await showCityWeather(div, localWeather);
+
+  const cityButton = document.querySelector("#submit");
+  // console.log(cityButton);
+
+  cityButton.addEventListener("click", async () => {
+    const cityInput = document.querySelector(".city");
+    const weather = await getWeatherByCity(cityInput.value);
+    await showCityWeather(div, weather);
+  });
+})();
+
+export async function showCityWeather(element, cityWeather) {
+  element.innerHTML = "";
+  const cityName = document.createElement("p");
+  cityName.innerText = `Showing weather for ${cityWeather.name}`;
+
+  // console.log(cityName);
+  element.appendChild(cityName);
+
+  const weather = document.createElement("p");
+  weather.innerText = `Current weather is ${cityWeather.main.temp} degrees of Celcium`;
+  // console.log(weather);
+  element.appendChild(weather);
+
+  const img = document.createElement("img");
+  const weatherCode = cityWeather.weather[0].icon;
+  img.src = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
+  // console.log(img);
+  element.appendChild(img);
+
+  const favicon = document.querySelector("#favicon");
+  favicon.href = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
+}
+
 export async function getWeatherByCity(cityName) {
   let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=458b6f24d9bec1db57c2f337dc35dbcc`;
   let response = await fetch(url);
   let json = await response.json();
 
-  console.log(json);
+  // console.log(json);
   return json;
 }
 
@@ -13,7 +53,7 @@ export async function getLocalWeather() {
   let url = "https://get.geojs.io/v1/ip/geo.json";
   let response = await fetch(url);
   let json = await response.json();
-  console.log(json);
+  // console.log(json);
 
   const lat = json.latitude;
   const lon = json.longitude;
@@ -21,30 +61,6 @@ export async function getLocalWeather() {
   response = await fetch(url);
   json = await response.json();
 
-  console.log(json);
+  // console.log(json);
   return json;
 }
-
-(async function () {
-  // const userCity = await getUserCity();
-  const currentCityWeather = await getLocalWeather();
-
-  const div = document.querySelector(".weather");
-
-  const cityName = document.createElement("p");
-  cityName.innerText = currentCityWeather.name;
-
-  console.log(cityName);
-  div.appendChild(cityName);
-
-  const weather = document.createElement("p");
-  weather.innerText = currentCityWeather.main.temp;
-  console.log(weather);
-  div.appendChild(weather);
-
-  const img = document.createElement("img");
-  const weatherCode = currentCityWeather.weather[0].icon;
-  img.src = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
-  console.log(img);
-  div.appendChild(img);
-})();
