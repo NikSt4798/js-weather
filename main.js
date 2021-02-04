4814e71fbe40df5ef38e57aad87b96139f89222b
NikSt4798 <NikSt4798@gmail.com>
2021-02-04 18:46:15 +0000

    Deploying to gh-pages from  @ 048fd105349686904f801a44466e6d18507d1748 🚀

--- main.js
+++ main.js
@@ -1,1 +1,73 @@
-(()=>{"use strict";var e={138:(e,t,n)=>{async function r(e){const t=document.querySelector(".weather");t.innerHTML="";const n=document.createElement("p");n.innerText=`Showing weather for ${e.name}`,t.appendChild(n);const r=document.createElement("p");r.innerText=`Current weather is ${e.main.temp} degrees of Celcium`,t.appendChild(r);const a=document.createElement("img"),c=e.weather[0].icon;a.src=`https://openweathermap.org/img/wn/${c}@2x.png`,t.appendChild(a),document.querySelector("#favicon").href=`https://openweathermap.org/img/wn/${c}@2x.png`;const o=document.querySelector(".map");o.innerHTML="";const i=document.createElement("img");i.src=`https://static-maps.yandex.ru/1.x/?ll=${e.coord.lon},${e.coord.lat}&size=450,450&z=11&l=map`,o.appendChild(i)}async function a(e){let t=`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${e}&appid=458b6f24d9bec1db57c2f337dc35dbcc`,n=await fetch(t);return await n.json()}!async function(){const e=await async function(){let e="https://get.geojs.io/v1/ip/geo.json",t=await fetch(e),n=await t.json();return e=`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${n.latitude}&lon=${n.longitude}&appid=458b6f24d9bec1db57c2f337dc35dbcc`,t=await fetch(e),n=await t.json(),n}();await r(e),document.querySelector("#submit").addEventListener("click",(async()=>{const e=document.querySelector(".city"),t=await a(e.value);await r(t),function(e){const t=document.querySelector(".history"),n=t.querySelectorAll("p");let c=!1;if(n.forEach((t=>{t.innerText!==e||(c=!0)})),c)return;const o=document.createElement("p");o.innerText=e,o.addEventListener("click",(async function(){const e=await a(this.innerText);await r(e)})),t.insertBefore(o,n[0]),n.length>10&&n[n.length-1].remove()}(e.value)}))}()}},t={};function n(r){if(t[r])return t[r].exports;var a=t[r]={exports:{}};return e[r](a,a.exports,n),a.exports}n.d=(e,t)=>{for(var r in t)n.o(t,r)&&!n.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},n.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),n(138)})();
+(() => {
+  "use strict";
+  async function e(e) {
+    let t = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${e}&appid=458b6f24d9bec1db57c2f337dc35dbcc`,
+      n = await fetch(t);
+    return await n.json();
+  }
+  async function t(n) {
+    const a = document.querySelector(".weather");
+    a.innerHTML = "";
+    const c = document.querySelector(".map");
+    c.innerHTML = "";
+    const i = document.createElement("p");
+    if (void 0 === n.name)
+      return (
+        (i.innerText = "Can't find this city, try again."),
+        void a.appendChild(i)
+      );
+    (i.innerText = `Showing weather for ${n.name}`), a.appendChild(i);
+    const r = document.createElement("p");
+    (r.innerText = `Current weather is ${n.main.temp} degrees of Celcium`),
+      a.appendChild(r);
+    const o = document.createElement("img"),
+      d = n.weather[0].icon;
+    (o.src = `https://openweathermap.org/img/wn/${d}@2x.png`),
+      a.appendChild(o),
+      (document.querySelector(
+        "#favicon"
+      ).href = `https://openweathermap.org/img/wn/${d}@2x.png`);
+    const s = document.createElement("img");
+    (s.src = `https://static-maps.yandex.ru/1.x/?ll=${n.coord.lon},${n.coord.lat}&size=450,450&z=11&l=map`),
+      c.appendChild(s),
+      (function (n) {
+        const a = document.querySelector(".history"),
+          c = a.querySelectorAll("p");
+        let i = !1;
+        if (
+          (c.forEach((e) => {
+            e.innerText !== n || (i = !0);
+          }),
+          i)
+        )
+          return;
+        const r = document.createElement("p");
+        (r.innerText = n),
+          r.addEventListener("click", async function () {
+            const n = await e(this.innerText);
+            await t(n);
+          }),
+          a.insertBefore(r, c[0]),
+          c.length >= 10 && c[c.length - 1].remove();
+      })(n.name);
+  }
+  !(async function () {
+    const n = await (async function () {
+      let e = "https://get.geojs.io/v1/ip/geo.json",
+        t = await fetch(e),
+        n = await t.json();
+      return (
+        (e = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${n.latitude}&lon=${n.longitude}&appid=458b6f24d9bec1db57c2f337dc35dbcc`),
+        (t = await fetch(e)),
+        (n = await t.json()),
+        n
+      );
+    })();
+    await t(n),
+      document.querySelector("#submit").addEventListener("click", async () => {
+        const n = document.querySelector(".city"),
+          a = await e(n.value);
+        await t(a);
+      });
+  })();
+})();
