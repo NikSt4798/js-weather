cec2420c0627dcabcbeb4ee3a07a1e5a634d1234
Nikita Stolbovskih <stolbovskikh@speechpro.com>
2021-02-26 23:37:36 +0300

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./dist"),
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "./dist"),
     compress: true,
     port: 9000,
   },
