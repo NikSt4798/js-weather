f46beb365f33fd621f490fb1dd569a4b6d241a39
Nikita Stolbovskih <stolbovskikh@speechpro.com>
2021-03-07 21:40:50 +0300

    change import and mock

--- src/onCityClick.test.js
+++ src/onCityClick.test.js
@@ -1,18 +1,20 @@
 import { onCityClick } from "./onCityClick";
-import * as getWeatherByCityModule from "./getWeatherByCity";
-import * as showCityWeatherModule from "./showCityWeather";
+import { getWeatherByCity } from "./getWeatherByCity";
+import { showCityWeather } from "./showCityWeather";
 
-jest
-  .spyOn(getWeatherByCityModule, "getWeatherByCity")
-  .mockImplementation(() => "weather");
+jest.mock("./getWeatherByCity", () => ({
+  getWeatherByCity: jest.fn(() => ({ cityName: "Moscow" })),
+}));
 
-jest.spyOn(showCityWeatherModule, "showCityWeather").mockImplementation();
+jest.mock("./showCityWeather", () => ({
+  showCityWeather: jest.fn(),
+}));
 
 describe("It should show weather for clicked city", () => {
   it("should show weather by clicking on city", async () => {
     await onCityClick();
 
-    expect(getWeatherByCityModule.getWeatherByCity).toBeCalled();
-    expect(showCityWeatherModule.showCityWeather).toBeCalledWith("weather");
+    expect(getWeatherByCity).toBeCalled();
+    expect(showCityWeather).toBeCalledWith({ cityName: "Moscow" });
   });
 });
