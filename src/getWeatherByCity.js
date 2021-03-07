f03f3234d79fc91145ea1d10d8ebf31de0b874f3
Nikita Stolbovskih <stolbovskikh@speechpro.com>
2021-03-07 22:54:07 +0300

    fix

--- src/getWeatherByCity.js
+++ src/getWeatherByCity.js
@@ -1,7 +1,7 @@
 export async function getWeatherByCity(cityName) {
-  let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=458b6f24d9bec1db57c2f337dc35dbcc`;
-  let response = await fetch(url);
-  let json = await response.json();
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=458b6f24d9bec1db57c2f337dc35dbcc`;
+  const response = await fetch(url);
+  const json = await response.json();
 
   return json;
 }
